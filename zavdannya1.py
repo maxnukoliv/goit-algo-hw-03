902714f5feeeb079d27b4f57fddda068b2eb111d
maxnukoliv <novacraft.wither1@gmail.com>
2025-01-15 18:11:59 +0200

    updated

--- zavdannya1.py
+++ zavdannya1.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-now = (datetime.today())
 date = '2020-10-09'
 
-def get_days_from_today(date, now):
+def get_days_from_today(date):
+    now = (datetime.today())
     date2 = datetime.strptime(date, "%Y-%m-%d")
     result = now - date2
-    result_final = result.day
+    result_final = result.days
     return result_final
 
-print(get_days_from_today(date, now))
+print(get_days_from_today(date))
--- zavdannya2.py
+++ zavdannya2.py
@@ -1,8 +1,13 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    list = range(min, max)
-    target = random.sample(list, quantity)
-    return(target)
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+    if min >= 1 and max <= 1000:
+        list = range(min, max)
+        target = random.sample(list, quantity)
+        return(sorted(target))
+    else:
+        target = []
+        return(target)
+lottery_numbers = get_numbers_ticket(1, 1200 , 4)
+
 print("Ваші лотерейні числа:", lottery_numbers)
