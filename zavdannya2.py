b3cdb36009ff238f231ff4165f027ab3d64589e8
MaxNukoliv <91463019+maxnukoliv@users.noreply.github.com>
2025-01-17 19:52:30 +0200

    Update2

--- zavdannya2.py
+++ zavdannya2.py
@@ -1,13 +1,17 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min >= 1 and max <= 1000:
+    if min >= max or min - max <= quantity:
+            target = []
+            return(target)
+    elif min >= 1 and max <= 1000 :
         list = range(min, max)
         target = random.sample(list, quantity)
         return(sorted(target))
     else:
         target = []
         return(target)
-lottery_numbers = get_numbers_ticket(1, 1200 , 4)
+
+lottery_numbers = get_numbers_ticket(10, 5, -1)
 
 print("Ваші лотерейні числа:", lottery_numbers)
